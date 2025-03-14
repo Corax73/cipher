251e937c1f2e44aca42270ac38c890398d83746f
Corax73 <ramaz999@inbox.ru>
2025-03-14 23:41:25 +0400

    work with file size

--- main.go
+++ main.go
@@ -9,7 +9,7 @@ import (
 	"os"
 )
 
-func encryptFile(key []byte, filepath string) error {
+func encryptFile(key []byte, filepath string, coef int64) error {
 	plaintext, err := os.ReadFile(filepath)
 	if err != nil {
 		return err
@@ -27,7 +27,7 @@ func encryptFile(key []byte, filepath string) error {
 	}
 
 	cfb := cipher.NewCBCEncrypter(block, iv)
-	ciphertext := make([]byte, aes.BlockSize*1000)
+	ciphertext := make([]byte, aes.BlockSize*coef)
 	cfb.CryptBlocks(ciphertext, plaintext)
 	ciphertext = append(iv, ciphertext...)
 
@@ -36,7 +36,7 @@ func encryptFile(key []byte, filepath string) error {
 	return os.WriteFile(filepath+".enc", []byte(encodedCiphertext), 0644)
 }
 
-func decryptFile(key []byte, filepath string) error {
+func decryptFile(key []byte, filepath string, coef int64) error {
 	os.ReadFile(filepath)
 	ciphertext, err := os.ReadFile(filepath)
 	if err != nil {
@@ -57,7 +57,7 @@ func decryptFile(key []byte, filepath string) error {
 	ciphertext = decodedCiphertext[aes.BlockSize:]
 
 	cfb := cipher.NewCBCDecrypter(block, iv)
-	plaintext := make([]byte, aes.BlockSize*1000)
+	plaintext := make([]byte, aes.BlockSize*coef)
 	cfb.CryptBlocks(plaintext, ciphertext)
 
 	return os.WriteFile(filepath+".enc", plaintext, 0644)
@@ -74,15 +74,25 @@ func main() {
 	// Choose between encryption or decryption
 	action := "encrypt" // or "decrypt"
 
+	file, err := os.Stat(filepath)
+	if err != nil {
+		fmt.Println("File not found:", err)
+	}
+	coef := file.Size()
+	if file.Size() > aes.BlockSize {
+		coef = (file.Size() % aes.BlockSize)
+	}
+	coef += 100
+
 	if action == "encrypt" {
-		err := encryptFile(key, filepath)
+		err := encryptFile(key, filepath, coef)
 		if err != nil {
 			fmt.Println("Error encrypting file:", err)
 			return
 		}
 		fmt.Println("File encrypted successfully!")
 	} else if action == "decrypt" {
-		err := decryptFile(key, filepath+".enc")
+		err := decryptFile(key, filepath+".enc", coef)
 		if err != nil {
 			fmt.Println("Error decrypting file:", err)
 			return
